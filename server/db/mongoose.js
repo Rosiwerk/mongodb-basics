5b0f99d4532b2ec2be21573d128654a07967a26f
Filip <fabianek.filip2@gmail.com>
2017-03-11 22:20:23 +0100

    Just some small change

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 // Promises aren't build in (callbacks are), but he likes to use them so...
 mongoose.Promise = global.Promise;
 db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
+  localhost: "mongodb://localhost:27017/TodoApp",
   mlab: "mongodb://rosiwerk:mamamijajo@ds129030.mlab.com:29030/mydatabase1"
 }
 mongoose.connect(db.mlab || db.localhost);
